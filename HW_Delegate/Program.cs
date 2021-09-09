d66d463f75153d4854158ee72c669acb0124ffb3
88jin@mail.ru <88jin@mail.ru>
2021-09-09 21:58:36 +0300

    начал дз2 на заятии

--- HW_Delegate/Program.cs
+++ HW_Delegate/Program.cs
@@ -10,7 +10,7 @@ namespace HW_Delegate
             int num1 = int.Parse(Console.ReadLine());
             int num2 = int.Parse(Console.ReadLine());
 
-            Aripfmetic aripfmeticSum = (num1, num2) => num1 + num2;          
+            Aripfmetic aripfmeticSum = (num1, num2) => num1 + num2;
 
             Aripfmetic aripfmetiсSub = (num1, num2) => num1 - num2;
 
@@ -23,21 +23,38 @@ namespace HW_Delegate
                     Console.WriteLine("на ноль делить нельзя");
                     return 0;
                 }
-                else
-                {
-                    return num1 / num2;
-                }
-            };
+                return num1 / num2;
 
-            
+            };
 
             Console.WriteLine(aripfmeticSum(num1, num2));
             Console.WriteLine(aripfmetiсSub(num1, num2));
             Console.WriteLine(aripfmeticMult(num1, num2));
             Console.WriteLine(aripfmeticDev(num1, num2));
 
-        }
-    }
 
-    
+            RandDelegat[] MassDelegats = new RandDelegat[]
+                {
+                    RandomNum,
+                    RandomNum,
+                    RandomNum,
+                    RandomNum,
+                    RandomNum
+                };
+            
+
+        }
+
+        delegate int RandDelegat();
+            
+
+        private static int RandomNum()
+        {
+            Random rand = new Random();
+            int x = rand.Next(1, 15);
+            return x;
+        }
+
+
+    } 
 }
