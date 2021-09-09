8f37ffcf518b785ba35dc393f4c5c82d20b82595
88jin@mail.ru <88jin@mail.ru>
2021-09-09 20:26:01 +0300

    Добавьте файлы проекта.

---
+++ HW_Delegate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HW_Delegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
