5f6ccc562ac59513eeed3a1a7ae0fc25d6e6e820
88jin@mail.ru <88jin@mail.ru>
2021-09-09 20:58:06 +0300

    Лямбдавыражения для арифметических действий.

--- HW_Delegate/Program.cs
+++ HW_Delegate/Program.cs
@@ -4,9 +4,40 @@ namespace HW_Delegate
 {
     class Program
     {
+        delegate int Aripfmetic(int num1, int num2);
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+
+            Aripfmetic aripfmeticSum = (num1, num2) => num1 + num2;          
+
+            Aripfmetic aripfmetiсSub = (num1, num2) => num1 - num2;
+
+            Aripfmetic aripfmeticMult = (num1, num2) => num1 * num2;
+
+            Aripfmetic aripfmeticDev = (num1, num2) =>
+            {
+                if (num2 == 0)
+                {
+                    Console.WriteLine("на ноль делить нельзя");
+                    return 0;
+                }
+                else
+                {
+                    return num1 / num2;
+                }
+            };
+
+            
+
+            Console.WriteLine(aripfmeticSum(num1, num2));
+            Console.WriteLine(aripfmetiсSub(num1, num2));
+            Console.WriteLine(aripfmeticMult(num1, num2));
+            Console.WriteLine(aripfmeticDev(num1, num2));
+
         }
     }
+
+    
 }
